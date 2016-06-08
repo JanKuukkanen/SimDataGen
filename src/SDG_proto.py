c34107e7a219d8f09a66942321f7051b87a8eb3a
JanKuukkanen <jankuukkanen@gmail.com>
2016-06-08 11:37:18 +0300

    Fixed stuff left over from merge conflict

--- src/SDG_proto.py
+++ src/SDG_proto.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Main functionality (Presentation layer)
 
 from SDG_bl import SimDataGen
@@ -39,20 +38,3 @@ try:
 
 except (KeyboardInterrupt, SystemExit):
 	sys.exit()
-=======
-from database_connect import DatabaseSession
-
-# initialize database object
-tmp_conn = DatabaseSession()
-
-# Connect to database
-tmp_conn.establish_connection()
-
-con_success = tmp_conn.get_connection()
-
-if (con_success == True):
-	
-	tmp_conn.send_current(1000, 1010)
-
-	tmp_conn.close_connection()
->>>>>>> upstream/master
--- src/data_format.py
+++ src/data_format.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Functions used for data formatting and validation
 
 import datetime
@@ -6,11 +5,5 @@ import datetime
 # Format datetime to a form fitting Cassandra's datetime type
 def get_time_format():
 	time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-=======
-import datetime
-
-def get_time_format():
-	time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
->>>>>>> upstream/master
 
 	return time
