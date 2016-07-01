b993439c4354e945ba3144adde1791bf244bf9dd
JanKuukkanen <jankuukkanen@gmail.com>
2016-07-01 11:13:49 +0300

    updated installation file

--- installation_sdg.sh
+++ installation_sdg.sh
@@ -1,8 +1,15 @@
 # installation file for SimDataGen
-apt-get install python2.7
 
-pip install -U pip
+chmod -R u+x .
 
-pip install cassandra-driver
+apt-get update
+
+apt-get -y install python2.7
+
+pip install -y -U pip
+
+pip install -y cassandra-driver
+
+apt-get -y install python-matplotlib
 
 python resources/sdg_install.py
